58e0236df253fa2ba4ad1cdf850597912c611893
victorgalam <victorgalam2000@gmail.com>
2024-11-06 10:17:57 +0200

    victor-alone

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+//routes/users.js
+
+import express from "express";
+import {
+    deleteUser,
+    getUser,
+    getUsers,
+    updateUser,
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/:id", getUser);
+router.get("/", getUsers);
+
+export default router;
